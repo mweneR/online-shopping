7ca9d9b2ccaf0c26c89669d554eacaf9558af92c
mweneRu <hmutabazi39@gmail.com>
2019-05-23 11:41:32 +0200

    my project setup completed successfully

---
+++ shoppingbackendreal/src/main/java/net/harold/shoppingbackendreal/App.java
@@ -0,0 +1,13 @@
+package net.harold.shoppingbackendreal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
